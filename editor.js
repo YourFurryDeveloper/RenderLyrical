0ab9396137746015791717505d0c899edaba8b45
McTechDev <thatonefurryreal@gmail.com>
2025-09-22 11:45:31 -0400

    Add fonts

--- editor.js
+++ editor.js
@@ -1,1 +1,8 @@
 createToolbar(35, "#242424", "#5c5c5c", "#2a3638", "white", "white", 7, "pointer", {"File":{"Open":"openEditMenu", "Save":"openFileMenu"}, "Edit":{"Copy":"openEditMenu", "Paste":"openFileMenu"}, "Tools":{"Delete CoAI":"deleteMsAI", "Pen":"ShowPenTool"}});
+
+document.getElementById("fontselector").addEventListener("change", function() {
+    if (document.getElementById("fontselector").value !== "nullFont") {
+        console.log(document.getElementById("fontselector").value)
+        document.getElementById("fontselector").style.fontFamily = document.getElementById("fontselector").value;
+    }
+})
