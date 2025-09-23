0bb0dec77fe5f06dd522f8627e860d0ac149824e
McTechDev <thatonefurryreal@gmail.com>
2025-09-23 14:59:21 -0400

    Working on keyframes

--- editor.js
+++ editor.js
@@ -1,5 +1,15 @@
 createToolbar(35, "#242424", "#5c5c5c", "#2a3638", "white", "white", 7, "pointer", {"File":{"Open":"openEditMenu", "Save":"openFileMenu"}, "Edit":{"Copy":"openEditMenu", "Paste":"openFileMenu"}, "Tools":{"Delete CoAI":"deleteMsAI", "Pen":"ShowPenTool"}});
 
+let projJsonFile = {
+    "projConf": {
+        "sceneBG": ""
+    },
+
+    "objects": {
+
+    }
+};
+
 
 function createObject() {
     let previewAreaFrame = document.getElementById("animPreview");
@@ -26,12 +36,10 @@ function createObject() {
     let newObjectDropdown = document.createElement("details");
     let newObjectDropdownName = document.createElement("summary");
     newObjectDropdown.style.marginLeft = "20px";
-    newObjectDropdown.style.marginBottom = "5px";
     newObjectDropdown.className = "objectDropdown";
-    newObjectDropdown.id = "objectDropdown_" + objectId;
+    newObjectDropdown.id = objectId.value;
     newObjectDropdownName.className = "objectDropdownSummary";
     newObjectDropdownName.textContent = objectId.value;
-    newObjectDropdownName.style.marginBottom = "10px";
 
     //let selectActiveBtn = document.createElement("button");
     //selectActiveBtn.textContent = "select";
@@ -61,6 +69,7 @@ function createObject() {
     document.querySelectorAll(".objectDropdown").forEach(dropdown => {
         dropdown.addEventListener("toggle", function () {
             if (this.open) {
+                loadObjectData(this.id);
                 // Close all other dropdowns
                 document.querySelectorAll(".objectDropdown").forEach(otherDropdown => {
                     if (otherDropdown !== this) {
@@ -71,13 +80,56 @@ function createObject() {
         });
     });
 
+    projJsonFile.objects[objectId.value] = {
+        "text": objectText.value,
+        "font": objectFont.value,
+        "fontSize": objectFontSize.value,
+        "color": objectTextColor.value,
+
+        "keyframes": {
+        }
+    }
+
+
+    objectTextEditor.addEventListener("change", function() {
+        projJsonFile.objects[newTxtObject.id]["text"] = objectTextEditor.value;
+        refreshPreviewObject(newTxtObject.id);
+    })
+
+    objectColorEditor.addEventListener("change", function() {
+        projJsonFile.objects[newTxtObject.id]["color"] = objectColorEditor.value;
+        refreshPreviewObject(newTxtObject.id);
+    })
+
 
     objectText.value = "";
     objectId.value = "";
 }
 
+function refreshPreviewObject(previewObjId) {
+    let previewAreaFrame = document.getElementById("animPreview");
+    let previewArea = previewAreaFrame.contentDocument || previewAreaFrame.contentWindow.document;
+
+    previewArea.getElementById(previewObjId).textContent = projJsonFile.objects[previewObjId]["text"];
+    previewArea.getElementById(previewObjId).style.color = projJsonFile.objects[previewObjId]["color"];
+    previewArea.getElementById(previewObjId).style.fontFamily = projJsonFile.objects[previewObjId]["font"];
+    previewArea.getElementById(previewObjId).style.fontSize = projJsonFile.objects[previewObjId]["fontSize"];
+}
+
+function loadObjectData(objId) {
+    document.getElementById("animCodeEditor").innerHTML = projJsonFile.objects[objId]["keyframes"];
+    document.getElementById("animCodeEditor").name = objId;
+}
+
+
 document.getElementById("fontselector").addEventListener("change", function() {
     if (document.getElementById("fontselector").value !== "nullFont") {
         document.getElementById("fontselector").style.fontFamily = document.getElementById("fontselector").value;
     }
+})
+
+document.getElementById("animCodeEditor").addEventListener("input", function() {
+    animCodeEditor = document.getElementById("animCodeEditor");
+
+    projJsonFile.objects[animCodeEditor.name]["keyframes"] = animCodeEditor.value;
 })
